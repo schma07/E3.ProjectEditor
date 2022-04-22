99ec5caba05e2274f287f9bdd3d794262f7b114b
schma07 <91978870+schma07@users.noreply.github.com>
2022-04-22 17:35:02 +0200

    Created basic Customer entity classes

---
+++ ProjectEditor.Core/Entities/Customers/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectEditor.Core.Entities.Customers
+{
+    public partial class Customer : CustomerBase
+    {
+        DateTime CreateDate { get; set; } = DateTime.Now;
+        DateTime? ModifyDate { get; set; }
+
+    }
+}
---
+++ ProjectEditor.Core/Entities/Customers/CustomerBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectEditor.Core.Entities.Customers
+{
+    public abstract class CustomerBase 
+    {
+        public virtual Guid Id { get; set; }   
+
+        public string Name { get; set; } = string.Empty;
+               
+    }
+}
