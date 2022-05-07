92732a37e3e025b044ef8000f7dcc91b4c2e95e8
schma07 <91978870+schma07@users.noreply.github.com>
2022-05-07 21:10:27 +0200

    Created queries for Device DTO

---
+++ ProjectEditor.Core/Application/Queries/GetDeviceDtoQuery.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using ProjectEditor.Core.Application.Results;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectEditor.Core.Application.Queries
+{
+    public class GetDeviceDtoQuery
+    {
+        public class GetMovieDtoQuery : IRequest<DeviceDto>
+        {
+            public Guid Id { get; set; }
+        }
+    }
+}
---
+++ ProjectEditor.Core/Application/Queries/GetDeviceDtosQuery.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using ProjectEditor.Core.Application.Results;
+using ProjectEditor.Core.Entities.Projects;
+using System;
+using System.Collections.Generic;
+
+namespace ProjectEditor.Core.Application.Queries
+{
+    public class GetDeviceDtosQuery : IRequest<IEnumerable<DeviceDto>>
+    {
+        public Project Project { get; set; }
+
+        public int Take { get; set; }
+        public int Skip { get; set; }
+    }
+}
