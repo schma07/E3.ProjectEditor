d851e7c8fbfd64b95484c68eefb62daa424e3a23
schma07 <91978870+schma07@users.noreply.github.com>
2022-05-20 22:16:42 +0200

    Added Controller for devices

---
+++ ProjectEditor.WebApp/Controllers/DeviceController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ProjectEditor.Core.Entities.Devices;
+using ProjectEditor.Persistence.Repositories.DBContext;
+
+namespace ProjectEditor.WebApp.Controllers
+{
+    public class DeviceController : Controller
+    {
+        private readonly ProjectEditorDbContext _context;
+
+        public DeviceController(ProjectEditorDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Device
+        public async Task<IActionResult> Index()
+        {
+            var projectEditorDbContext = _context.Devices.Include(d => d.Project);
+            return View(await projectEditorDbContext.ToListAsync());
+        }
+
+        // GET: Device/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var device = await _context.Devices
+                .Include(d => d.Project)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (device == null)
+            {
+                return NotFound();
+            }
+
+            return View(device);
+        }
+
+        // GET: Device/Create
+        public IActionResult Create()
+        {
+            ViewData["ProjectId"] = new SelectList(_context.Projects, "Id", "Id");
+            return View();
+        }
+
+        // POST: Device/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,ProjectId")] Device device)
+        {
+            if (ModelState.IsValid)
+            {
+                device.Id = Guid.NewGuid();
+                _context.Add(device);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProjectId"] = new SelectList(_context.Projects, "Id", "Id", device.ProjectId);
+            return View(device);
+        }
+
+        // GET: Device/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var device = await _context.Devices.FindAsync(id);
+            if (device == null)
+            {
+                return NotFound();
+            }
+            ViewData["ProjectId"] = new SelectList(_context.Projects, "Id", "Id", device.ProjectId);
+            return View(device);
+        }
+
+        // POST: Device/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,Name,ProjectId")] Device device)
+        {
+            if (id != device.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(device);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DeviceExists(device.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProjectId"] = new SelectList(_context.Projects, "Id", "Id", device.ProjectId);
+            return View(device);
+        }
+
+        // GET: Device/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var device = await _context.Devices
+                .Include(d => d.Project)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (device == null)
+            {
+                return NotFound();
+            }
+
+            return View(device);
+        }
+
+        // POST: Device/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var device = await _context.Devices.FindAsync(id);
+            _context.Devices.Remove(device);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DeviceExists(Guid id)
+        {
+            return _context.Devices.Any(e => e.Id == id);
+        }
+    }
+}
